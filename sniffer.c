431dcc2db1932d4f500905448c5661308eecc945
Sir J <therealdev_fa@icloud.com>
2026-04-29 21:43:55 +0300

    Update sniffer.c

--- sniffer.c
+++ sniffer.c
@@ -1,16 +1,17 @@
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pcap.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <netinet/udp.h>
-#include <netinet/ip_icmp.h>
 #include <arpa/inet.h>
 #include <signal.h>
 #include <errno.h>
 #include <unistd.h>
+
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/in.h>
 
 #define DEFAULT_PROTOCOL    "tcp"
 
